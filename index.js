7b6115f4cf2948541e5f3ba436d5e717c1dc94bd
jolly <vasanth_r@outlook.com>
2023-02-17 16:32:25 +0530

    Adds durationInput, startButton, pauseButton dom objects to index.js. Creates timer object. Tests skeleton code. It works.

--- index.js
+++ index.js
@@ -14,3 +14,11 @@ class Timer {
         console.log('Time to start the timer!');
     }
 }
+
+// HTML dom access
+const durationInput = document.querySelector('#duration');
+const startButton = document.querySelector('#start');
+const pauseButton = document.querySelector('#pause');
+
+// Timer Object
+const timer = new Timer(durationInput, startButton, pauseButton);
